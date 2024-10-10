bf0877b6ce9040074e567d26b103862d7ab7d9e2
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-10 11:56:12 +0530

    Update Pattern.py

--- Pattern.py
+++ Pattern.py
@@ -1,36 +1,132 @@
-# Full Pyramid of Stars
+# Full Pyramid of Stars 
+#              * 
+#             * * 
+#            * * * 
+#           * * * * 
+#          * * * * * 
+#           * * * * 
+#            * * * 
+#             * * 
+#              *
+
 def full_pyramid(n):
     print("\nFull Pyramid of Stars")
     for i in range(n):
         print(' ' * (n - i - 1) + '* ' * (i + 1))
 
+#..................................................................................................................
+
+
 # Inverted Pyramid of Stars
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
 def inverted_pyramid(n):
     print("\nInverted Pyramid of Stars")
     for i in range(n, 0, -1):
         print(' ' * (n - i) + '* ' * i)
+#..................................................................................................................
 
 # Right-Angled Triangle
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
 def right_angled_triangle(n):
     print("\nRight-Angled Triangle of Stars")
     for i in range(1, n + 1):
         print('* ' * i)
+        
+#..................................................................................................................
+
 
 # Inverted Right-Angled Triangle
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
 def inverted_right_angled_triangle(n):
     print("\nInverted Right-Angled Triangle of Stars")
     for i in range(n, 0, -1):
         print('* ' * i)
+        
+#..................................................................................................................
+
 
 # Diamond Pattern
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
 def diamond(n):
     print("\nDiamond Pattern")
     for i in range(n):
         print(' ' * (n - i - 1) + '* ' * (i + 1))
     for i in range(n - 1, 0, -1):
         print(' ' * (n - i) + '* ' * i)
+        
+#..................................................................................................................
+
 
 # Hollow Square
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
 def hollow_square(n):
     print("\nHollow Square Pattern")
     for i in range(n):
@@ -39,7 +135,21 @@ def hollow_square(n):
         else:
             print('* ' + '  ' * (n - 2) + '*')
 
+#..................................................................................................................
+
 # Number Pyramid
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
 def number_pyramid(n):
     print("\nNumber Pyramid")
     for i in range(1, n + 1):
@@ -48,7 +158,22 @@ def number_pyramid(n):
             print(j, end=' ')
         print()
 
+
+#..................................................................................................................
+
 # Pascal’s Triangle
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
 def pascals_triangle(n):
     print("\nPascal's Triangle")
     prev = []
@@ -59,6 +184,9 @@ def pascals_triangle(n):
         print(' ' * (n - i), end='')
         print(' '.join(map(str, row)))
         prev = row
+
+#..................................................................................................................
+
 
 # Set n value
 n = 5
@@ -73,7 +201,7 @@ hollow_square(n)
 number_pyramid(n)
 pascals_triangle(n)
 
-
+#..................................................................................................................
 
 
 
